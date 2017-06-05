6f6ba7cc7d2c299c3f1315ea468bc394f0ad2884
Audora LeeAnn <scribeklio@gmail.com>
2017-06-04 21:40:02 -0400

    initial commit

---
+++ src/components/BodyRibbon.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class BodyRibbon extends Component {
+  render() {
+    return (
+      <div className="BodyRibbon">
+ 
+      </div>
+    );
+  }
+}
+
+export default BodyRibbon;
